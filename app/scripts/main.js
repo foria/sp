fd419c2228ba865717172933b602a9b06ab466e8
Marco Melluso <foriaa@gmail.com>
2018-11-19 18:46:29 +0800

    html home, 1st version of header & footer

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+jQuery(function($){
+
+    $(document).ready(function(){
+        $('body').on('click', '.nav-trigger', function() {
+            $('body').toggleClass('menu-opened');
+            $('.nav-menu').fadeToggle(200);
+        });
+
+        // $(document).scroll(function (e) {
+        //   $.each($('section'), function (index, section) {
+        //     if($(this).scrollTop() >= section.getBoundingClientRect().top && $(this).scrollTop() <= section.getBoundingClientRect().bottom){
+        //       if ($(section).hasClass('bgwhite')) {
+        //         $('.js_navbar').removeClass('nav-trigger');
+        //         $('.js_navbar').addClass('nav-trigger-dark');
+        //       } else {
+        //         $('.js_navbar').removeClass('nav-trigger-dark');
+        //         $('.js_navbar').addClass('nav-trigger');
+        //       }
+        //     }
+        //   });
+        // });
+    })
+
+})
