9c545d1d2946adb4544758734908511cd109cef0
Marco Melluso <foriaa@gmail.com>
2018-11-23 19:59:38 +0800

    home ready for desktop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,6 @@ AOS.init({
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 300, // values from 0 to 3000, with step 50ms
@@ -52,19 +51,19 @@ jQuery(function($){
             }
         });
 
-        // $(document).scroll(function (e) {
-        //   $.each($('section'), function (index, section) {
-        //     if($(this).scrollTop() >= section.getBoundingClientRect().top && $(this).scrollTop() <= section.getBoundingClientRect().bottom){
-        //       if ($(section).hasClass('bgwhite')) {
-        //         $('.js_navbar').removeClass('nav-trigger');
-        //         $('.js_navbar').addClass('nav-trigger-dark');
-        //       } else {
-        //         $('.js_navbar').removeClass('nav-trigger-dark');
-        //         $('.js_navbar').addClass('nav-trigger');
-        //       }
-        //     }
-        //   });
-        // });
+        if(window.innerWidth > 1024) {
+            window.addEventListener('scroll', function(e) {
+                if ($(document).scrollTop() > 300) {
+                    $('body').addClass("sticky-header");
+                    $('.wheel').removeClass("animate");
+                    $('#cd-vertical-nav').addClass('show-nav');
+                } else {
+                    $('body').removeClass("sticky-header");
+                    $('.wheel').addClass("animate");
+                    $('#cd-vertical-nav').removeClass('show-nav');
+                }
+            });
+        }
 
         // Scroll Navigation
         var contentSections = $('.cd-section'),
